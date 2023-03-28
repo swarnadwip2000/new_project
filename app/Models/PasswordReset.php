bba85e64e2dbeb448b09bf268d0f6387e22a1eab
excellisit <web.excellisit@gmail.com>
2023-03-28 19:01:45 +0530

    problem solve

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class PasswordReset extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    protected $casts = [ 'created_at'=>'datetime'];
+
+    protected $fillable = [
+        'email',
+        'token',
+        'created_at',
+    ];
 }
